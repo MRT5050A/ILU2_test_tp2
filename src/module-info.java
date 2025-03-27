b55d92f8d3f894fdde4d03f1d28cb2ae769f545e
MRT5050A <titouan.martineau@univ-tlse3.fr>
2025-03-27 16:56:54 +0100

    rdm

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ILU2_test_tp2 {
+	requires org.junit.jupiter.api;
+	requires junit;
+}
---
+++ test/controleur/ControlEmmenagerTest.java
@@ -0,0 +1,24 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import villagegaulois.Village;
+
+class ControlEmmenagerTest {
+	
+	Village village;
+	
+	@BeforeEach
+	void init() {
+		ControlEmmenager control = new ControlEmmenager(null)
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
